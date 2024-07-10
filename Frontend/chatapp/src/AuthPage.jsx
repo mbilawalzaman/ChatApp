4c0a367cbbf8cb04265bd3fc9334fd7afe948224
Muhammad Bilawal Zaman <143614305+mbilawalzaman@users.noreply.github.com>
2024-07-10 23:01:12 +0000

    updated

--- Frontend/chatapp/src/AuthPage.jsx
+++ Frontend/chatapp/src/AuthPage.jsx
@@ -1,14 +1,28 @@
+import React from "react";
 import axios from "axios";
 
 const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
-    const { value } = e.target[0];
+    const username = e.target.username.value;
+
+    // Call onAuth immediately with the username (if needed)
+    props.onAuth({ username: username, secret: username });
+
+    // Make axios POST request to authenticate
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
-      .then((r) => props.onAuth({ ...r.data, secret: value }))
-      .catch((e) => console.log("Auth Error", e));
+      .post("http://localhost:3001/authenticate", { username: username })
+      .then((response) => {
+        const responseData = response.data;
+        const user = { ...responseData, secret: username };
+        props.onAuth(user); // Update onAuth with server response
+      })
+      .catch((error) => {
+        console.log("Auth Error", error);
+      });
   };
+
+  console.log("hello", props);
 
   return (
     <div className="background">
---
+++ Frontend/chatapp/src/ChatsPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ChatsPage = () => {
+  return <div className="background">chats...</div>;
+};
+export default ChatsPage;
